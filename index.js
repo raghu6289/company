40b6d7fa196ab2c3f475101d080c9045d3031aa8
raghu6289 <raghua7795@gmail.com>
2022-07-21 19:25:11 +0530

    files added

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require('dotenv/config')
 const express = require('express')
-const db = require('./Models')
+const db = require('./app/models/index')
 const app = express()
 
-
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Welcome to Node")
 })
 
+app.use("/api/v1/company", mainRouter)
+
 const port = 3000
 
 const start = async () => {
-    await db.sequelize.sync({ force: false });
+    await db;
     app.listen(port, () => console.log(`server is runningc at port ${port}`))
     db.department.create({ name: 'test' })
 }
